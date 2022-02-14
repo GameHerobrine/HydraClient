68b7f5193cae722245bdbb235394a904cdc180a4
moderatorman <12193288+moderatorman@users.noreply.github.com>
2022-02-14 13:27:21 -0800

    Set correct status bools

--- src/minecraft/com/oldschoolminecraft/client/Client.java
+++ src/minecraft/com/oldschoolminecraft/client/Client.java
@@ -27,6 +27,8 @@ public class Client {
 		eventManager = new EventManager();
 		perkChecker = new PerkChecker();
 		perkChecker.fetchPerks(mc.session.username);
+		isSupporter = perkChecker.getPerkList().contains("supporter_menu");
+		isStaff = perkChecker.getPerkList().contains("staff_menu");
 		System.out.print(isSupporter);
 
 		if(mc.session.username.contains("Player"))
