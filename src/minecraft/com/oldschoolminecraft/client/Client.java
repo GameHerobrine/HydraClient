c389c748c269be9d623b1d6bc1f5ecc1a131e577
moderatorman <12193288+moderatorman@users.noreply.github.com>
2022-02-27 08:47:42 -0800

    Fix Client.java

--- src/minecraft/com/oldschoolminecraft/client/Client.java
+++ src/minecraft/com/oldschoolminecraft/client/Client.java
@@ -1,11 +1,7 @@
 package com.oldschoolminecraft.client;
 
-import org.lwjgl.input.Keyboard;
+import com.oldschoolminecraft.client.perks.PerkManager;
 import com.oldschoolminecraft.client.event.EventManager;
-import com.oldschoolminecraft.client.event.EventTarget;
-import com.oldschoolminecraft.client.event.events.EventKeyboard;
-import com.oldschoolminecraft.client.gui.GuiClientOptions;
-import com.oldschoolminecraft.client.perks.PerkChecker;
 import com.oldschoolminecraft.client.updater.Updater;
 
 import net.minecraft.client.Minecraft;
@@ -22,11 +18,8 @@ public class Client {
 	//Client
 	public String version = "b0.01";
 	public String name = "OSM Client";
-	public boolean isSupporter = false;
-	public boolean isDebugMode = false;
-	public boolean isStaff = false;
 	public boolean isUpdateAvailable = false;
-	public PerkChecker perkChecker;
+	public PerkManager perkManager;
 	public Updater updater;
 	public EventManager eventManager;
 
@@ -34,12 +27,9 @@ public class Client {
 		instance = this;
 		mc = Minecraft.getMinecraft();
 		eventManager = new EventManager();
-		perkChecker = new PerkChecker();
+		perkManager = new PerkManager();
 		updater = new Updater();
-		perkChecker.fetchPerks(mc.session.username);
-		isSupporter = perkChecker.getPerkList().contains("supporter_menu");
-		isStaff = perkChecker.getPerkList().contains("staff_menu");
-		isDebugMode = perkChecker.getPerkList().contains("debug");
-		updater.checkUpdates(version);
+		perkManager.fetchPerks(mc.session.username);
+		updater.checkUpdates(version, (flag) -> isUpdateAvailable = flag);
 	}
 }
