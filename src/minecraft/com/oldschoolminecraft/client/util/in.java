9e3827ac5d17f30c8ee05d05efda10275638bfc0
moderatorman <12193288+moderatorman@users.noreply.github.com>
2022-03-20 12:39:05 -0700

    Start & in

---
+++ src/minecraft/com/oldschoolminecraft/client/util/in.java
@@ -0,0 +1,9 @@
+package com.oldschoolminecraft.client.util;// this is needed for the sound engine to work with deobfuscated sourcecode without crashing
+
+public class in {
+    public static int b(double d)
+    {
+        int i = (int)d;
+        return d >= (double)i ? i : i - 1;
+    }
+}
